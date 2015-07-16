16a260a639ada5f775dbaf7ff5a1b640774a70a9
Ivan Manolov <ivanmanolov90@gmail.com>
2015-07-16 14:00:43 -0400

    adding cssClassManipulation

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -1,5 +1,24 @@
 var cssClass = (function() {
-  'use strict';
+    'use strict';
 
-  // code goes here
+    // cssClass.add(htmlElement, className);
+    // cssClass.remove(htmlElement, className);
+    // cssClass.toggle(htmlElement, className);
+    // cssClass.has(htmlElement, className);
+
+    return {
+    	add: function (htmlElement, className) {
+        return htmlElement.className += ' ' + className;
+    	},
+    	remove: function(htmlElement, className) {
+    	    return htmlElement.className = htmlElement.className.replace(className, '');
+    	},
+    	toggle: function (htmlElement, className) {
+    		return (this.has(htmlElement, className)) ? this.remove(htmlElement, className) : this.add(htmlElement, className);
+    	},
+    	has: function (htmlElement, className) {
+    		return (htmlElement.className.match(className)) ? true : false;
+    	}
+
+    }
 })();
